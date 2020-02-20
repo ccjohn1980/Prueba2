c27e9871a88924da214b01445da4aa3576715342
l11m01 <l11m01@SURCO.com>
2020-02-20 18:53:31 -0500

    Esto es otra prueba

---
+++ AppPrueba/src/appprueba/Factura.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appprueba;
+
+/**
+ *
+ * @author l11m01
+ */
+public class Factura {
+    
+    public static void main (String[] args){
+    
+    int codigo;
+    double subtotal = 0, total;
+    
+    int precio;
+    int cantidad;
+    
+    total = subtotal + (subtotal*0.18);
+    
+System.out.println("el total es "+total);
+    
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+}
